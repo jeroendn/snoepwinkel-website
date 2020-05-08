be7e12fb3b1f1510218bf339ad141493f2d458ca
jeroendn <51778824+jeroendn@users.noreply.github.com>
2020-05-08 16:14:47 +0200

    New DB user

--- php/dbconnection.inc.php
+++ php/dbconnection.inc.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "remotemysql.com";
-$dBUsername = "sEdqkIHteW";
-$dBPassword = "za2hPp6sIR";
-$dBName = "sEdqkIHteW";
+$servername = "web0098.zxcs.nl";
+$db_username = "u71481p69034_projects";
+$db_password = "PycIWi64";
+$db_name = "u71481p69034_snoepwinkel";
 
 try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dBName",$dBUsername,$dBPassword);
+	$conn = new PDO("mysql:host=$servername;dbname=$db_name",$db_username,$db_password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e)
@@ -13,12 +13,12 @@ catch(PDOException $e)
   ?><p style="display: block; background: #f00; color: #fff; text-align: center; margin-bottom: 0;">Couldn't connenct to database: <?php echo $e->getMessage();?><br>Connecting to a local database...</p><?php
 
 	$servername = "localhost";
-	$dBUsername = "root";
-	$dBPassword = "";
-	$dBName = "snoepwinkel";
+	$db_username = "root";
+	$db_password = "";
+	$db_name = "snoepwinkel";
 
 	try {
-		$conn = new PDO("mysql:host=$servername;dbname=$dBName",$dBUsername,$dBPassword);
+		$conn = new PDO("mysql:host=$servername;dbname=$db_name",$db_username,$db_password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		?><p style="display: block; background: #f00; color: #fff; text-align: center; margin-bottom: 0;">Connected to the local database!</p><?php
 	}
